b36f8f224fd316a4bebdb3bfeef7b5529bed57f2
Robert Lesser <rdlesser@gmail.com>
2023-01-26 23:01:18 +0200

    MazeTilesList serializable

--- Assets/Scripts/ScriptableObjects/MazeTilesList.cs
+++ Assets/Scripts/ScriptableObjects/MazeTilesList.cs
@@ -1,9 +1,9 @@
-﻿
-
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MazeTilesList", menuName = "ScriptableObjects/MazeTilesList", order = 1)]
+[Serializable]
 public class MazeTilesList : ScriptableObject
 {
         public List<MazeTileData> TileDatas;
